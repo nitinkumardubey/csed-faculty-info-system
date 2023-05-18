a1cd4848364283da3d511373e0ec7d7284b96dea
Nitin Kumar Dubey <80614507+nitinkumardubey@users.noreply.github.com>
2023-05-18 18:57:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieparser = require('cookie-parser');
 dotenv.config({ path: './config.env' });
 app.use(cookieparser());
 app.use(express.json({ limit: '50mb' }));
+const path = require("path");
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 8000;
@@ -18,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
      app.use(express.static("client/build"));
 }
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
      res.sendFile(path.join(__dirname, './client/build/index.html'), function (err) {
           if (err) {
                res.status(500).send(err)
