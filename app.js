0cbab69b52632490550a3b03e86fffc6626ca3a9
Nitin Kumar Dubey <80614507+nitinkumardubey@users.noreply.github.com>
2023-05-18 18:35:09 +0530

    update app.js
    added
    
    app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, './client/build/index.html'), function (err) {
              if (err) {
                   res.status(500).send(err)
              }
         })
    })

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ if (process.env.NODE_ENV === "production") {
      app.use(express.static("client/build"));
 }
 
+app.get("*", (req, res) => {
+     res.sendFile(path.join(__dirname, './client/build/index.html'), function (err) {
+          if (err) {
+               res.status(500).send(err)
+          }
+     })
+})
+
 app.listen(port, (req, res) => {
      console.log(`server running at ${port}`);
 });
