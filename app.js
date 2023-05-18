866c76d5d5eb5196371e2f588a76bc73c9291830
Nitin Kumar Dubey <80614507+nitinkumardubey@users.noreply.github.com>
2023-05-18 18:52:59 +0530

    update app.js
    changed get form * to /
    and client to ./client

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ if (process.env.NODE_ENV === "production") {
      app.use(express.static("client/build"));
 }
 
-app.get("*", (req, res) => {
-     res.sendFile(path.join(__dirname, 'client/build/index.html'), function (err) {
+app.get("/", (req, res) => {
+     res.sendFile(path.join(__dirname, './client/build/index.html'), function (err) {
           if (err) {
                res.status(500).send(err)
           }
